de6d8069416d718b084d2d3cc965a742376241c9
只是一杯咖啡 <17688528794@163.com>
2024-11-03 16:12:31 +0800

    fix: 添加全局异常处理

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { HttpExceptionsFilter } from 'src/common/filters/http-exceptions-filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalFilters(new HttpExceptionsFilter());
   await app.listen(3000);
 }
 bootstrap();
